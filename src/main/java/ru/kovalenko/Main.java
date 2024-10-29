9312bdcdbb6be83be0f5da416ebe7a7a012f1b02
Roman <astek14@mail.ru>
2024-10-30 00:48:50 +0300

    test parsing JSON

--- src/main/java/ru/kovalenko/Main.java
+++ src/main/java/ru/kovalenko/Main.java
@@ -1,7 +1,27 @@
 package ru.kovalenko;
 
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Yandex Weather Api");
+
+        String content = null;
+        try {
+            content = new String(Files.readAllBytes(Paths.get("example" + File.separator + "weather.json")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // https://yandex.ru/dev/weather/doc/ru/concepts/forecast-info
+        JSONObject jsonObject = new JSONObject(content);
+        int now = jsonObject.getInt("now");
+        JSONObject fact = jsonObject.getJSONObject("fact");
+        int temp = fact.getInt("temp");
+        System.out.println(temp);
     }
 }
